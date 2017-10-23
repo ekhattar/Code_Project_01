143f0304aa0ee651acc7e043d07a7a888561c911
Max Ropohl <m.ropohl@outlook.de>
2017-10-23 14:14:20 +0200

    gitignore geupdated, comments in ingame hinzugefügt

--- Software_Development/src/components/Ingame.vue
+++ Software_Development/src/components/Ingame.vue
@@ -1,9 +1,13 @@
 <template>
+
     <div v-if="isCounterStarted">
+        <!-- Navbar -->
         <ul>
             <li v-if="false" class="life" v-for="life in lifes" v-text="life"></li>
             <li class="score">Score: {{ score }}</li>
         </ul>
+
+        <!-- Question -->
         <div v-if="!isQuestionDone">
             <p style="text-align: center;">Wie ist die Hauptstadt?</p>
             <br>
@@ -15,9 +19,10 @@
                 <li class="ab"><button class="btn" @click="click('D')">D</button></li>
             </ul>
         </div>
+
+        <!-- Result -->
         <div v-else>
             <p style="text-align: center">Correct Answer</p>
-
             <ul>
                 <li>
                     <button class="normal" @click="nextQuestion()">Next Question</button>
@@ -27,111 +32,109 @@
                 </li>
             </ul>
         </div>
+
     </div>
 
+    <!-- Countdown -->
     <div v-else class="container1">
-
-            <p>{{counter}}</p>
-
+        <p>{{counter}}</p>
     </div>
 
 
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                name: 'ingame',
-                lifes: ['O', 'O', 'O'],
-                score: 0,
-                time: 15,
-                isCounterStarted: false,
-                isQuestionDone: false,
-                counter: 3
-            }
-        },
+export default {
+  data() {
+    return {
+      name: "ingame",
+      lifes: ["O", "O", "O"],
+      score: 0,
+      time: 15,
+      isCounterStarted: false,
+      isQuestionDone: false,
+      counter: 3
+    };
+  },
 
-        methods: {
-            click(answer) {
-                if(answer === 'A') {
-                    this.isQuestionDone = true;
-                }
-            },
-            nextQuestion() {
-                this.isQuestionDone = false;
-            }
-        },
-
-        created() {
-            setInterval(() => {
-               // console.log(this.counter);
-//                console.log('seconds');
-                this.counter -= 1;
-                //console.log(this.counter);
-                if (this.counter === -1) {
-                    console.log("HAPPY NEW YEAR!!");
-                    this.isCounterStarted = true;
-                }
-            }, 1000);
-        }
+  methods: {
+    click(answer) {
+      if (answer === "A") {
+        this.isQuestionDone = true;
+      }
+    },
+    nextQuestion() {
+      this.isQuestionDone = false;
     }
+  },
 
+  created() {
+    setInterval(() => {
+      // console.log(this.counter);
+      //                console.log('seconds');
+      this.counter -= 1;
+      //console.log(this.counter);
+      if (this.counter === -1) {
+        console.log("HAPPY NEW YEAR!!");
+        this.isCounterStarted = true;
+      }
+    }, 1000);
+  }
+};
 </script>
 
 <style>
+.normal {
+  width: 300px;
+  height: 20px;
+}
 
-    .normal {
-        width: 300px;
-        height: 20px;
-    }
+.btn {
+  width: 300px;
+  height: 40px;
+}
 
-    .btn{
-        width: 300px;
-        height: 40px;
-    }
+.ab {
+  margin: 0;
+  padding: 0;
+}
+div.container1 {
+  height: 10em;
+  position: relative;
+}
 
-    .ab {
-        margin: 0;
-        padding: 0;
-    }
-    div.container1 {
-        height: 10em;
-        position: relative
-    }
+div.container1 p {
+  margin: 0;
+  background: yellow;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+}
 
-    div.container1 p {
-        margin: 0;
-        background: yellow;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%)
-    }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  height: 100px;
+}
 
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        height: 100px;
-    }
+li {
+  font-size: 20px;
+  text-align: center;
+  margin-left: 0;
+}
 
-    li {
-        font-size: 20px;
-        text-align: center;
-        margin-left: 0;
-    }
+.life {
+  width: 80px;
+  margin: 10px;
+}
 
-    .life {
-        width: 80px;
-        margin: 10px;
-    }
+.score {
+}
 
-    .score {
-    }
-
-    .time {
-        margin-left: 4em;
-    }
+.time {
+  margin-left: 4em;
+}
 </style>
