fb62d35398b0345eeba977bfeaeff66f30a4f578
Max Ropohl <m.ropohl@outlook.de>
2017-10-26 13:04:17 +0200

    added new buttons to welcome

--- Software_Development/src/components/Welcome.vue
+++ Software_Development/src/components/Welcome.vue
@@ -2,45 +2,49 @@
 	<div>
 		<h1>Welcome</h1>
 		<router-link to="/quiz" class="buttonlink"> 
-			 Play
+			<uiButton title="Play"></uiButton> 
 		</router-link>
-		<router-link to="/leaderboard" class="buttonlink"> 
-			 Leaderboard
+		<router-link to="/leaderboard" class="buttonlink">
+            <uiButton title="Leaderboard"></uiButton> 
 		</router-link>
 	</div>
 </template>
 
 <script>
-import Http from '../http-rest.js';
-	import {Event} from '../event.js'
-	 export default {
-	 	data() {
-	 		return {
-	 			name: 'welcome',
-	 		}
-	 	},
-	 	methods: {
-	 		changeTemp(destTemp) {
-	 			console.log("clicked")
-	 			Event.$emit('change',destTemp);
-	 			console.log(destTemp)
-	 		}
-	 	},
-	 	// mounted() {
-	 	// 		   //quiz-question?questionId=3&questionId=4&questionId=5
-	  //   Http.get(`/quiz-question?questionId=100`)
-	  //   	.then(response => {
-	  //     		//JSON responses are automatically parsed.
-	  //     		//response.addHeader("Access-Control-Allow-Origin", "*");
-	  //     		this.posts = response.data;
-	  //     		console.log(this.posts)
-	  //   	})
-	  //   		.catch(e => {
-	  //     		this.errors.push(e);
-	  //   	})
-		 	
-		 // }
-	}
+import Http from "../http-rest.js";
+import { Event } from "../event.js";
+import uiButton from "./ui_elements/Button.vue";
+export default {
+    components: {
+        uiButton
+    },
+    data() {
+        return {
+            name: "welcome"
+        };
+    },
+    methods: {
+        changeTemp(destTemp) {
+            console.log("clicked");
+            Event.$emit("change", destTemp);
+            console.log(destTemp);
+        }
+    }
+    // mounted() {
+    // 		   //quiz-question?questionId=3&questionId=4&questionId=5
+    //   Http.get(`/quiz-question?questionId=100`)
+    //   	.then(response => {
+    //     		//JSON responses are automatically parsed.
+    //     		//response.addHeader("Access-Control-Allow-Origin", "*");
+    //     		this.posts = response.data;
+    //     		console.log(this.posts)
+    //   	})
+    //   		.catch(e => {
+    //     		this.errors.push(e);
+    //   	})
+
+    // }
+};
 </script>
 
 <style scoped lang="sass">
