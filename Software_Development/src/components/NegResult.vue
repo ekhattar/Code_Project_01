f9aaaf12e9095f590594e0261bb8416a88f35e6d
Elias Khattar <khattarelias@gmail.com>
2017-10-26 17:32:46 +0200

    implemented welcome and results

--- Software_Development/src/components/NegResult.vue
+++ Software_Development/src/components/NegResult.vue
@@ -1,14 +1,12 @@
 <template>
 	<div>
-        <p style="text-align: center">Incorrect Answer</p>
+        <p style="text-align: center" class="incorrect"><span class="white">Sorry,</br></span> WRONG!</p>
         <p style="text-align: center">You lost 1 Life</p>
-        <uiButton v-bind:onClick="onNextQuestion" title="Next Question"></uiButton>
+        <uiButton class="btn3" v-bind:onClick="onNextQuestion" title="Next Question"></uiButton>
     </div>
 </template>
 
-<style>
 
-</style>
 
 <script>
 import { Event } from "../event.js";
@@ -26,3 +24,35 @@ export default {
 };
 </script>
 
+<style lang="sass">
+    .btn3
+        position: absolute
+        bottom: 24px
+        left: 50%
+        transform: translateX(-50%)
+        display: block
+    .incorrect
+        /* Sorry, WRONG! */
+
+        position: absolute;
+        width: 237px;
+        height: 98px;
+        left: 69px;
+        top: 164px;
+
+        font-family: Roboto Slab;
+        font-style: normal;
+        font-weight: bold;
+        line-height: normal;
+        font-size: 48px;
+        text-align: center;
+        color: #ED3964
+        text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+
+    .white
+        color: #FFFFFF;
+        font-size: 32px
+        font-weight: normal;
+
+
+</style>
--- Software_Development/src/components/PosResult.vue
+++ Software_Development/src/components/PosResult.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
-        <p style="text-align: center">Correct Answer</p>
+	<div class="container-result">
+        <p style="text-align: center" class="correct"><span class="white">Yeah that’s,</br></span> CORRECT!</p>
         <p style="text-align: center">You got {{ gain }} points!</p>
-        <ul>
-            <uiButton v-bind:onClick="onNextQuestion" title="Next Question"></uiButton>
-        </ul>
+        
+            <uiButton class="btn2" v-bind:onClick="onNextQuestion" title="Next Question"></uiButton>
+        
     </div>
 </template>
 
@@ -23,9 +23,36 @@ export default {
 };
 </script>
 
-<style>
-.normal {
-    width: 300px;
-    height: 20px;
-}
+
+<style lang="sass">
+    .btn2
+        position: absolute
+        bottom: 24px
+        left: 50%
+        transform: translateX(-50%)
+        display: block
+
+    .correct
+        /* Yeah that’s, CORRECT! */
+
+        position: absolute;
+        width: 260px;
+        height: 98px;
+        left: 59px;
+        top: 164px;
+
+        font-family: Roboto Slab;
+        font-style: normal;
+        font-weight: bold;
+        line-height: normal;
+        font-size: 42px;
+        text-align: center;
+
+        color: #1FB363;
+        text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+    .white
+        color: #FFFFFF;
+        font-weight: normal;
+        font-size: 32px;
+
 </style>
